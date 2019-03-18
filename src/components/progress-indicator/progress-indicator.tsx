2e627167d8d12a1e3335c99c41c56c68caa4f8b7
Yinon Oved <yinon@hotmail.com>
2019-03-18 12:21:08 +0200

    progress indicator inherits color

--- src/components/progress-indicator/progress-indicator.tsx
+++ src/components/progress-indicator/progress-indicator.tsx
@@ -13,8 +13,9 @@ export class OAIProgressIndicator {
 
     render() {
         return (
-            [<div class="lds-ripple"><div></div><div></div></div>]
-            // <div class="loader"></div>
+            <svg class="spinner" viewBox="0 0 50 50">
+                <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+            </svg>
         )
     }
 }
